d098446ada7346b5be4f380968943df3ab46025f
Uchemmy007 <frankmvson@gamil.com>
2024-10-12 08:58:17 -0700

    new function

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -3,3 +3,6 @@ number2 = 5
 print(f'Addition of {number1} and {number2} is {number1 + number2}')
 print(f'Substraction of {number1} and {number2} is {number1 - number2}')
 print(f'Multiplication of {number1} and {number2} is {number1 * number2}')
+
+def run():
+    print()
